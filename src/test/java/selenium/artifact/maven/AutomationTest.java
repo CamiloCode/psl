d1726733b815d2fe9c687e215e5b64f082b20258
Camilo <camilobackz@hotmail.com>
2017-01-15 23:37:49 -0500

    fix indentation issues

--- src/test/java/selenium/artifact/maven/AutomationTest.java
+++ src/test/java/selenium/artifact/maven/AutomationTest.java
@@ -6,12 +6,12 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.Test;
- 
+
 public class AutomationTest {
- 
+
 	@Test
 	public void TestFireFox(){
-	  
+
 		System.setProperty("webdriver.gecko.driver", "geckodriver.exe");	
 		WebDriver driver = new FirefoxDriver();
 		driver.manage().window().maximize();
@@ -19,26 +19,26 @@ public class AutomationTest {
 		PageObject.agendarCitas(driver).click();
 		PageObject.datePicker(driver).click();
 		driver.quit();
- 
+
 	}
-	
-	
+
+
 	public void TestChrome(){
-	  System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
-	  // To remove message "You are using an unsupported command-line flag: --ignore-certificate-errors.
-	  // Stability and security will suffer."
-	  // Add an argument 'test-type'
-	  DesiredCapabilities capabilities = DesiredCapabilities.chrome();
-	  ChromeOptions options = new ChromeOptions();
-	  options.addArguments("test-type");
-	  options.addArguments("start-maximized");
-	  //capabilities.setCapability("chrome.binary","chromedriver.exe");
-	  capabilities.setCapability(ChromeOptions.CAPABILITY, options);
-	  WebDriver driver = new ChromeDriver(capabilities);
+		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+		// To remove message "You are using an unsupported command-line flag: --ignore-certificate-errors.
+		// Stability and security will suffer."
+		// Add an argument 'test-type'
+		DesiredCapabilities capabilities = DesiredCapabilities.chrome();
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("test-type");
+		options.addArguments("start-maximized");
+		//capabilities.setCapability("chrome.binary","chromedriver.exe");
+		capabilities.setCapability(ChromeOptions.CAPABILITY, options);
+		WebDriver driver = new ChromeDriver(capabilities);
 		driver.manage().window().maximize();
 		driver.get("http://automatizacion.herokuapp.com/ocontreras/");
 		driver.quit();
- 
+
 	}
- 
+
 }
--- src/test/java/selenium/artifact/maven/PageObject.java
+++ src/test/java/selenium/artifact/maven/PageObject.java
@@ -9,18 +9,18 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 public class PageObject {
 
 	private static WebElement element = null;
-    
+
 	public static WebElement agendarCitas(WebDriver driver){
 
-	  element = driver.findElement(By.cssSelector(".list-group-item:nth-of-type(6)"));
+		element = driver.findElement(By.cssSelector(".list-group-item:nth-of-type(6)"));
 		return element;
 
 	}
-		
+
 	public static WebElement datePicker(WebDriver driver){
 
-	  WebDriverWait wait = new WebDriverWait(driver, 15);
-	  wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("datepicker")));
+		WebDriverWait wait = new WebDriverWait(driver, 15);
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("datepicker")));
 		element = driver.findElement(By.id("datepicker"));
 		return element;
 
