e54865d46feba6dc9e0b72bd46dbf32d76d81910
Camilo <camilobackz@hotmail.com>
2017-01-15 23:29:19 -0500

    small change

---
+++ src/test/java/selenium/artifact/maven/PageObject.java
@@ -0,0 +1,28 @@
+package selenium.artifact.maven;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class PageObject {
+
+	private static WebElement element = null;
+    
+	public static WebElement agendarCitas(WebDriver driver){
+
+	  element = driver.findElement(By.cssSelector(".list-group-item:nth-of-type(6)"));
+		return element;
+
+	}
+		
+	public static WebElement datePicker(WebDriver driver){
+
+	  WebDriverWait wait = new WebDriverWait(driver, 15);
+	  wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("datepicker")));
+		element = driver.findElement(By.id("datepicker"));
+		return element;
+
+	}
+}
